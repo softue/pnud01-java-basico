75c4a96f1bf30d1a3c6846668b3176a8625e1e77
softue <romulomendesfigueiredo@gmail.com>
2023-10-20 20:28:03 -0300

    JFo 2 - Exercícios

---
+++ workspace-eclipse/JFo_2_exercicios/src/ProblemSet2_2.java
@@ -0,0 +1,15 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+    	// Um carrinho de Fórmula 1
+        System.out.println("X__/\\__X");
+        System.out.println("X |  | X");
+        System.out.println("  |  |  ");
+        System.out.println(" |    | ");
+        System.out.println(" |    | ");
+        System.out.println("X_    _X");
+        System.out.println("X______X");
+        System.out.println(" XXXXXX ");
+    } 
+}
