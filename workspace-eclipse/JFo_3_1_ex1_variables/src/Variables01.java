496b3b06a0cf1ea7be90236d95672402660bb0bc
softue <romulomendesfigueiredo@gmail.com>
2023-10-21 05:59:11 -0300

    JFo 3-1 - Exercícios com variáveis.

---
+++ workspace-eclipse/JFo_3_1_ex1_variables/src/Variables01.java
@@ -0,0 +1,16 @@
+public class Variables01 {
+
+    public static void main(String[] args) {
+        String x = "Rômulo";
+        System.out.println("My name is " +x);
+        System.out.println(x +" is so cool!");
+        System.out.println("Hooray " +x +"!");
+        System.out.println("Please enjoy " +x +" Appreciation \n"
+                + "Day! My name is " +x +". I know how excited \n"
+                + "everyone is to start appreciating " +x +" on " +x + "\n"
+                + " Appreciation Day! " +x +", " +x +", " +x +"! Yay \n"
+                + x +"!!! That's me! " +x +" is the best date ever!");
+        
+    }
+    
+}
