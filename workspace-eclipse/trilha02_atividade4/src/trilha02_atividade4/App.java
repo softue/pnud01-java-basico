5dca6273dc5972ffc6f7e5f7f7e221fc3dd4ecfb
softue <romulomendesfigueiredo@gmail.com>
2023-11-01 18:38:41 -0300

    PNUD: atividade 4 do caderno de atividades da trilha 2.

---
+++ workspace-eclipse/trilha02_atividade4/src/trilha02_atividade4/App.java
@@ -0,0 +1,11 @@
+package trilha02_atividade4;
+
+public class App {
+
+	public static void main(String[] args) {
+		//Caso caso = new Caso(12, 'C', "Crime");
+		Caso caso = new Caso(12, "C", "Crime");
+		System.out.println(caso.getDescricao());
+	}
+
+}
