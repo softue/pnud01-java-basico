aafaba4d4b968f54fb45aeb1999da9db2d58c441
softue <romulomendesfigueiredo@gmail.com>
2023-11-03 21:02:46 -0300

    JFo 7-2: Instanciando Objetos

---
+++ workspace-vscode/JFo_7_2_instanciando_objetos/src/Prisoner.java
@@ -0,0 +1,28 @@
+public class Prisoner {
+    private String nome;
+    private double altura;
+    private double sentenca;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double getSentenca() {
+        return sentenca;
+    }
+    public void setSentenca(double sentenca) {
+        this.sentenca = sentenca;
+    }
+
+    public void Pensar() {
+    }
+
+}
